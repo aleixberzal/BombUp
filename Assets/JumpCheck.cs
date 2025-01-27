7fa27da94fca0bc86b625ebc8cca4352c3de29e3
aleixberzal <aleix.berzal@enti.cat>
2025-01-27 10:50:09 +0100

    MovingFiles

---
+++ Assets/JumpCheck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCheck : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    int jumpPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetButtonDown("Jump")) {
+
+            Debug.Log("Salto");
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+
+        }
+        
+    }
+}
