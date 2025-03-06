9aa94a98941f36e69aa1d1193ae635b59e2bc83e
Víctor Solanes Quesada <victor.solanes@enti.cat>
2025-03-06 13:44:36 +0100

    bomba 3 done, falten dellats

--- Assets/Scripts/detonarBomba.cs
+++ Assets/Scripts/detonarBomba.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class detonarBomba : MonoBehaviour
 {
-    public string tagBuscado = "Bomba2";
+    public string tagBomb2 = "Bomba2";
+    public string tagBomb3 = "Bomba3";
 
 
-    
     void Update()
     {
         // Buscar un objeto con el tag "Bomba2"
-        GameObject bomba = GameObject.FindGameObjectWithTag(tagBuscado);
+        GameObject bomb2 = GameObject.FindGameObjectWithTag(tagBomb2);
 
         // Si existe una bomba y se presiona la tecla L
-        if (bomba != null && (Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.X)))
+        if (bomb2 != null && (Input.GetKeyDown(KeyCode.K) || Input.GetKeyDown(KeyCode.X)))
         {
             // Obtener el componente Explosiones del GameObject
-            Explosiones Explosiones = bomba.GetComponent<Explosiones>();
-            BombaPegajosa bombaPegajosa = bomba.GetComponent<BombaPegajosa>();
+            Explosiones Explosiones = bomb2.GetComponent<Explosiones>();
+            BombaPegajosa bombaPegajosa = bomb2.GetComponent<BombaPegajosa>();
 
             // Si el componente existe, ejecutar la explosión
             if (Explosiones != null && bombaPegajosa != null && bombaPegajosa.pegado)
@@ -30,6 +30,24 @@ public class detonarBomba : MonoBehaviour
             {
                 Debug.Log("El script Explosiones no está asignado a la bomba.");
             }
+        }
+
+        GameObject bomb3 = GameObject.FindGameObjectWithTag(tagBomb3);
+        if (bomb3 != null && (Input.GetKeyDown(KeyCode.L) || Input.GetKeyDown(KeyCode.C)))
+        {
+            Explosiones Explode3 = bomb3.GetComponent<Explosiones>();
+            fireworkBomb fireworkBomb = bomb3.GetComponent<fireworkBomb>();
+
+            if (Explode3 != null && fireworkBomb != null && fireworkBomb.ready)
+            {
+                fireworkBomb.SetReady(true);
+                bomb3.GetComponent<Collider2D>().enabled = false;
+            }
+            else
+            {
+                Debug.Log("El script Explosiones no está asignado a la bomba.");
+            }
+
         }
     }
 }
