29a9bb7023d1d5f55c38c5eeb2f741dfda6d6eef
Víctor Solanes Quesada <victor.solanes@enti.cat>
2025-03-12 23:42:53 +0100

    nova branch + %progress
    # Conflicts:
    #	Assembly-CSharp.csproj
    #	Assets/Bomba Activa.prefab

---
+++ Assets/Scripts/Progress.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Progress : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform player;
+    public Transform meta;
+    private float startHeight;
+    private float finalHeight;
+    private float actualHeight;
+    private float progreso;
+    public Text progressText;
+    void Start()
+    {
+        if (player != null)
+        {
+            startHeight = player.position.y;
+            finalHeight = meta.position.y;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            actualHeight = player.position.y;
+            progreso = (actualHeight - startHeight) / (finalHeight - startHeight) * 100;
+            progreso = Mathf.Clamp(progreso, 0, 100); // Asegura que no pase del 100%
+
+            progressText.text = progreso.ToString("F1") + "%";
+        }
+        
+    }
+}
