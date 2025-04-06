e8dfe03742d329e8152349682359c1fd06486c0e
EricPont <eric.pont@enti.cat>
2025-04-06 20:32:15 +0200

    Señales hechas (falta añadir sprite/texto E) - C4 rango y flecha arreglados

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject mensajeUI; // cartel de "Presiona E"
+    private bool jugadorCerca = false;  
+    private bool signActivada = false;
+    public GameObject turorial;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+    
+
+    void Update()
+    {
+        if (jugadorCerca && Input.GetKeyDown(KeyCode.E))
+        {
+            signActivada = true;
+            turorial.SetActive(true);
+            mensajeUI.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorCerca = true;
+            if (mensajeUI != null && signActivada == false) mensajeUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorCerca = false;
+            if (mensajeUI != null) mensajeUI.SetActive(false);
+        }
+    }
+}
