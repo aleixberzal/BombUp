6212ca7dbe496141254a0df2fd02ffff7bc56bdb
Víctor Solanes Quesada <victor.solanes@enti.cat>
2025-04-28 09:09:01 +0200

    bomba 1 collider fix

--- Assets/Scripts/Bombs/TestBomba.cs
+++ Assets/Scripts/Bombs/TestBomba.cs
@@ -5,31 +5,17 @@ using UnityEngine;
 public class TestBomba : MonoBehaviour
 {
 
-    [SerializeField] private Explosiones explosiones;
-    [SerializeField] private Color targetColor = Color.red; // Color al que cambiará el objeto
-    [SerializeField] private float changeDuration = 0.3f;    // Duración del cambio de color
-    [SerializeField] private float tiempo = 3f;          // Intervalo entre cambios
-
-    private Renderer objectRenderer;                       // Referencia al Renderer del objeto
-    private Color originalColor;                           // Color original del objeto
+    //private Rigidbody2D rb;
+    //public bool pegado = false;
+    [SerializeField] private Explosiones explosiones;  
+    [SerializeField] private float tiempo = 3f;     
 
     public float currentTime = 0;
     public bool hasExploded = false;
 
     void Start()
     {
-        // Obtener el Renderer del objeto
-        objectRenderer = GetComponent<Renderer>();
-
-        if (objectRenderer != null)
-        {
-            // Guardar el color original del material
-            originalColor = objectRenderer.material.color;
-        }
-        else
-        {
-            Debug.LogError("No se encontró un Renderer en el objeto.");
-        }
+        //rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -44,5 +30,33 @@ public class TestBomba : MonoBehaviour
         }
         
     }
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (!pegado && collision.gameObject.CompareTag("Suelo"))
+    //    {
+    //        pegado = true;
+    //        rb.velocity = Vector2.zero; // Detiene el movimiento
+    //        rb.isKinematic = true; // Desactiva la física para que no caiga
+    //        rb.angularVelocity = 0f;
+
+    //        // Ajustar la rotación según la normal de la superficie
+    //        Vector2 normal = collision.contacts[0].normal;
+    //        float angle = Mathf.Atan2(normal.y, normal.x) * Mathf.Rad2Deg;
+    //        transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+    //    }
+
+    //    if (!pegado && collision.gameObject.CompareTag("ParedBomba2"))
+    //    {
+    //        pegado = true;
+    //        rb.velocity = Vector2.zero;
+    //        rb.isKinematic = true;
+    //        rb.angularVelocity = 0f;
+
+    //        Vector2 normal = collision.contacts[0].normal;
+    //        float angle = Mathf.Atan2(normal.y, normal.x) * Mathf.Rad2Deg;
+    //        transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+    //    }
+    //}
 
 }
